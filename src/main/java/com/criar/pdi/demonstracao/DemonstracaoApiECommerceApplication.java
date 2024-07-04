208a81f7dd8a7349070fe385518e5b9efac5f9de
Mario Alberto <112214759+MrMa-rio@users.noreply.github.com>
2024-07-04 07:31:50 -0300

    Initial commit

---
+++ src/main/java/com/criar/pdi/demonstracao/DemonstracaoApiECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.criar.pdi.demonstracao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemonstracaoApiECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemonstracaoApiECommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/criar/pdi/demonstracao/DemonstracaoApiECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.criar.pdi.demonstracao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemonstracaoApiECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
