be3b61c76fb5a26c200d68a2c98f7354a3357fae
Mario Alberto <112214759+MrMa-rio@users.noreply.github.com>
2024-09-02 15:21:22 -0300

    feat: adding TODO for methods in Coupons Utilities

--- src/main/java/com/criar/pdi/demonstracao/components/CouponUtilities/CouponUtilities.java
+++ src/main/java/com/criar/pdi/demonstracao/components/CouponUtilities/CouponUtilities.java
@@ -17,20 +17,101 @@ public class CouponUtilities {
         return LocalDate.now().isAfter(couponDTO.expirationDate());
     }
 
-    public boolean validationType(ICouponGenericType couponGenericType, CouponCommonDTO couponCommonDTO){
-        if(couponGenericType.getClass() == CouponCreateType.class){
+    public boolean validationType(ICouponGenericType couponGenericType, CouponCommonDTO couponCommonDTO) {
+        if (couponGenericType.getClass() == CouponCreateType.class) {
+            if (couponGenericType == CouponCreateType.ADMIN) {
+                return true; /*
+                TODO: - Primeiro:
+                    Crie a logica desta condição;
+                TODO: - Segundo:
+                    Estruture para se adequar ao design pattern da aplicação;
+                */
+
+            }
+            if (couponGenericType == CouponCreateType.CLIENT) {
+                return true; /*
+                TODO: - Primeiro:
+                    Crie a logica desta condição;
+                TODO: - Segundo:
+                    Estruture para se adequar ao design pattern da aplicação;
+                */
+
+            }
+            if (couponGenericType == CouponCreateType.OWNER) {
+                return true; /*
+                TODO: - Primeiro:
+                    Crie a logica desta condição;
+                TODO: - Segundo:
+                    Estruture para se adequar ao design pattern da aplicação;
+                */
+
+            }
+        }
+        if (couponGenericType.getClass() == CouponDiscountType.class) {
+
+            if (couponGenericType == CouponDiscountType.PERCENTAGE) {
+                return true; /*
+                TODO: - Primeiro:
+                    Crie a logica desta condição;
+                TODO: - Segundo:
+                    Estruture para se adequar ao design pattern da aplicação;
+                */
+
+            }
+            if (couponGenericType == CouponDiscountType.FIXED) {
+                return true; /*
+                TODO: - Primeiro:
+                    Crie a logica desta condição;
+                TODO: - Segundo:
+                    Estruture para se adequar ao design pattern da aplicação;
+                */
+
+            }
 
         }
-        if(couponGenericType.getClass() == CouponDiscountType.class){
+        if (couponGenericType.getClass() == CouponType.class) {
 
-        }
-        if (couponGenericType.getClass() == CouponType.class){
+            if (couponGenericType == CouponType.CATEGORY) {
+                return true; /*
+                TODO: - Primeiro:
+                    Crie a logica desta condição;
+                TODO: - Segundo:
+                    Estruture para se adequar ao design pattern da aplicação;
+                */
 
+            }
+            if (couponGenericType == CouponType.EVENT) {
+                return true; /*
+                TODO: - Primeiro:
+                    Crie a logica desta condição;
+                TODO: - Segundo:
+                    Estruture para se adequar ao design pattern da aplicação;
+                */
+
+            }
+            if (couponGenericType == CouponType.STORE) {
+                return true; /*
+                TODO: - Primeiro:
+                    Crie a logica desta condição;
+                TODO: - Segundo:
+                    Estruture para se adequar ao design pattern da aplicação;
+                */
+
+            }
+            if (couponGenericType == CouponType.CLIENT) {
+                return true; /*
+                TODO: - Primeiro:
+                    Crie a logica desta condição;
+                TODO: - Segundo:
+                    Estruture para se adequar ao design pattern da aplicação;
+                */
+
+            }
         }
         return false;
     }
 
-    public boolean validationUsageCoupon(){
+    public boolean validationUsageCoupon() {
         return false;
     }
 
