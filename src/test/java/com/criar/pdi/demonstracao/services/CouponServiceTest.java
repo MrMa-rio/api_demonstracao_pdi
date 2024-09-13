04d40220e72386c6ffb580878774afb00518599a
Mario Alberto <112214759+MrMa-rio@users.noreply.github.com>
2024-09-13 17:58:27 -0300

    Update CouponServiceTest.java

--- src/test/java/com/criar/pdi/demonstracao/services/CouponServiceTest.java
+++ src/test/java/com/criar/pdi/demonstracao/services/CouponServiceTest.java
@@ -1,12 +1,28 @@
 package com.criar.pdi.demonstracao.services;
 
 import com.criar.pdi.demonstracao.DTOs.Coupon.CouponCommonDTO;
+import com.criar.pdi.demonstracao.DTOs.Coupon.CouponDTO;
 import com.criar.pdi.demonstracao.models.Coupon.Coupon;
 import com.criar.pdi.demonstracao.repositories.ICouponRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class CouponServiceTest {
@@ -16,7 +32,43 @@ public class CouponServiceTest {
     @Mock
     private ICouponRepository iCouponRepository;
     @Mock
-    private Coupon coupon;
+    private Coupon couponMock;
     @Mock
     private CouponCommonDTO couponCommonDTO;
+
+    @Test
+    @DisplayName("Teste")
+    void shouldReturnCoupon(){
+
+        Coupon coupon = new Coupon(
+                new CouponDTO(
+                        1,
+                        "1COUPOMTESTE",
+                        1,
+                        "",
+                        10.00,
+                        1,
+                        LocalDate.parse("2020-10-10"),
+                        null,
+                        null,
+                        null,
+                        null,
+                        null,
+                        1,
+                        LocalDateTime.now()
+
+                )
+        );
+
+        // Arrange
+        when(iCouponRepository.findById(anyInt())).thenReturn(Optional.of(coupon));
+
+        // Act
+        CouponCommonDTO result = couponService.getCouponByID("3");
+
+        // Assert
+        assertNotNull(result);
+        //verify(iCouponRepository).findById(2);
+
+    }
 }
